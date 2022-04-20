bac7d435d6f74f15ca3ab5524c1f4b69976cbfeb
Deiver Rovira <deiver2005@gmail.com>
2022-04-19 22:15:21 -0400

    FYI
    FYI

--- src/main/java/com/tbp/crud/entity/User.java
+++ src/main/java/com/tbp/crud/entity/User.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//Quizas lo sabias, pero este Data hace parte de una libreria llamada lombok que le agrega los getter/ setter de todos los atributos de la clase por defecto, osea q no tienes que ddefinir los 
+//getId, getFnName... etc
 @Data
 //crea un un constructor sin argumento
 @NoArgsConstructor
